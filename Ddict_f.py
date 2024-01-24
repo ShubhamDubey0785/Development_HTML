11b002d138970ffb169f9b74067c19537f802912
Shubham Dubey <shubhamdubey0785@gmail.com>
2024-01-24 21:51:42 +0530

    initial commit

---
+++ Ddict_f.py
@@ -0,0 +1,10 @@
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+newDict={k: sample_dict[k] for k in keys}
+print(newDict)
