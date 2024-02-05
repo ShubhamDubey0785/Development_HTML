12b54c0ae60592d9a6bc156f5e47d16ec84d807a
Shubham Dubey <shubhamdubey0785@gmail.com>
2024-02-06 00:14:45 +0530

    Initial commit

---
+++ tuple_.py
@@ -0,0 +1,22 @@
+# tuple1 = (10, 20, 30, 40, 50)
+# Expected output:
+# (50, 40, 30, 20, 10)
+# print(tuple1[::-1])
+# tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+# print(tuple1[1][1])
+# tuple2 = (10, 20, 30, 40)
+# print(tuple2[0])
+# print(tuple2[1])
+# print(tuple2[2])
+# print(tuple2[3])
+# tuple1 = (11, 22)
+# tuple2 = (99, 88)
+# tuple3=tuple1
+# tuple1=tuple2
+# tuple2=tuple3
+# print(tuple1)
+# print(tuple2)
+# tuple1 = (11, [22, 33], 44, 55)
+# tuple1[1][0]=222
+# print(tuple1)
+tuple1 = (('a', 23),('b', 37),('c', 11), ('d',29))
