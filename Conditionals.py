daeb544439660c4d252ae8ae26b2fc87048dc48e
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-02-29 23:56:02 +0530

    Add files via upload

---
+++ Conditionals.py
@@ -0,0 +1,48 @@
+# age=16
+# if(age>=18):
+#     print("Can vote")
+#     print("Can drive")
+
+# num=5
+# if(num>2):
+#     print("greater than 2") # indentation
+# elif(num>3):
+#     print("greater than 3")
+# else:
+#     print("numer is ",num)
+
+# marks=int(input("Enter students marks: "))
+# if(marks>=90):
+#     print("A")
+# elif(marks>=80 and marks<90):
+#     print("B")
+# elif(marks>=70 and marks<80):
+#     print("C")
+# else:
+#     print("S")
+
+
+# Write a program to check if a number entered by the user is odd or even.
+# num1=int(input("Enter a number: "))
+# if(num1%2==0):
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# Write a program to find the greatest of 3 number entered by the user.
+# n1=int(input("Enter first number: "))
+# n2=int(input("Enter second number: "))
+# n3=int(input("Enter third number: "))
+# if(n1>n2 and n1>n3):
+#     print(n1)
+# elif(n2>n1 and n2>n3):
+#     print(n2)
+# else:
+#     print(n3)
+
+# Write a program to check if a number is a multiple of 7 or not.
+mul=int(input("Enter your number: "))
+if(mul%7==0):
+    print("multiple of 7")
+else:
+    print("not a multiple of 7")
