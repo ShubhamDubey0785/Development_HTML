28e7fad5ed03ce3a21b6bd199acd237f84f6e0d5
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-02-29 23:56:30 +0530

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,66 @@
+# light=input("Light: ")
+# if(light=="red"):
+#     print("stop")
+# elif(light=="yellow"):
+#     print("look")
+# elif(light=="green"):
+#     print("go")
+# else:
+#     print("light is broken")
+
+
+
+# marks=input("marks: ")
+# if(marks>=90):
+#     print("A")
+# elif(marks>=80 and marks<90):
+#     print("B")
+# elif(marks>=70 and marks<80):
+#     print("C")
+# else:
+#     print("D")
+
+
+
+# A=5 & G=M
+# A=2 & G=F
+# A=int(input("A: "))
+# G=input("M/F: ")
+# if((A==1 or A==2) and G=="M"):
+#     print("fee is 100")
+# elif(A==3 or A==4 or G=="F"):
+#     print("fees is 200")
+# elif(A==5 and G=="M"):
+#     print("fee is 300")
+# else:
+#     print("no fee")
+
+# food=input("Food: ")
+# eat="Yes" if food=="cake" else "no"
+# print(eat)
+
+# food=input("food: ")
+# print("sweet") if food=="cake" or food=="jalebi" else print("not sweet")
+
+# age=int(input("Age: "))
+# vote=("yes","no")[age<18]
+# print(vote)
+
+sal=float(input("Salary: "))
+tax=sal*(0.1,0.2)[sal>5000]
+print(tax)
+
+
+# Simple interest calculate
+p=float(input("p: "))
+r=float(input("r: "))
+t=float(input("t: "))
+si=(p*r*t)/100
+print(si)
+
+# comments
+# Single line comment
+"""
+    Multi 
+    line comment
+"""
