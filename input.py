db5c967e080fd0f4f68f5e79400fc177db4566a5
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-03-17 22:04:55 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,33 @@
+# input() statement is used to accept values(using keyboard) from the user
+# name=input("Enter your name: ")
+# print("Welcome to the world of programming Mr./Mrs... ",name)
+
+# num=int(input("Enter an integer number: "))
+# print("Integer number entered by the user is: ",num)
+
+# dec=float(input("Enter a floating point number: "))
+# print("Float number entered by the user is: ",dec)
+
+# Let's practice
+# Write a Program to input 2 numbers & print their sum
+# num1=int(input("Enter num1: "))
+# num2=int(input("Enter num2: "))
+# sum=num1+num2
+# print("Sum of the number is",sum)
+
+# Write a program to input side of the square & print its area
+# side=int(input("Enter the side of the square: "))
+# print("Area of the square is: ",side*side)
+
+# Write a program to input 2 floating point numbers & print their average
+# n1=float(input("Enter first floating number: "))
+# n2=float(input("Enter second floating number: "))
+# print((n1+n2)/2)
+
+# Write a program to input 2 integer numbers & print true if a is greater than or equal to b, If not print False.
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if(a>=b):
+    print("True")
+else:
+    print("False")
