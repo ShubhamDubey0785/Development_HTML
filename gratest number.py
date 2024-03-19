f38fe27a5c2c9335e021625b31b2c36ef6626f59
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-03-20 00:52:49 +0530

    Add files via upload

---
+++ gratest number.py
@@ -0,0 +1,12 @@
+#take two input number from user(int/float)
+#print the greatest number
+
+userA = float(input("put first number"))
+userB = float(input("put second number"))
+if userA > userB:
+    print("A")
+
+elif userA < userB :
+    print("not true")
+elif userA == userB:
+    print("C")
