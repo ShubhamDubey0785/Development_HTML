786a646127302fec7c14c5e250991a6add4b7e2b
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-03-19 18:04:44 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,77 @@
+# Types of operator
+# Arithmetic Operator => +, -, *, /, //, %, **
+# Relational Operator => ==, !=, >, <, >=, <=
+# Assignment Operator =, -=, *=, /=, %=, //=, **=
+# Logical Operator (not, and, in)
+# Membership Operator (in, not in)
+# Identity Operator (is, is not)
+# Bitwise operator (&, |, ^)
+
+# Question.?  Hint. using operator precedence
+# print(not True and False or True)
+
+# Arithmetic operator
+# a=5
+# b=2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+# relational operator
+# a=50
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a>b)
+# print(a<=b)
+# print(a<b)
+
+# Assignment operator
+# num=10
+# num=num+10
+# num+=10
+# num-=10
+# num*=10
+# num/=10
+# num%=10
+# print("num: ",num)
+# num=10
+# num**=5
+# print("num: ",num)
+
+# Logical operators
+# print(not True)
+# print(not False)
+# a=50
+# b=30
+# print(not (a>b))
+
+# val1=True
+# val2=True
+# print("and operator: ", val1 and val2)
+# print("or operator: ", val1 or val2)
+
+# Type conversion
+# a,b=1,2.25
+# sum=a+b
+# print(sum)
+
+# Error
+# a,b=1,"2"
+# sum=a+b
+
+# Type casting
+# a,b=1,"2"
+# b=int(b)
+# sum=a+b
+# print(sum)
+
+# a=3.14
+# a=str(a)
+# print(a)
+# print(type(a))
+
